27db61145bb4e7063ba7026c0972eeafb4a8c5dc
Sneha Sadhwani <snehasadhwani@hotmail.com>
2026-06-03 13:26:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from libs.email_alert import send_security_email
 from libs.secrets import secrets
 from libs.utils import format_time
 
-# in
+# initialize wifi and try to set time
 init_wifi(secrets["ssid"], secrets["password"])
 
 try:
@@ -19,25 +19,18 @@ try:
 except:
     pass
 
-i2c = I2C(
-    0,
-    sda=Pin(0),
-    scl=Pin(1),
-    freq=10000
-)
-
+# set up i2c and lcd
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=10000)
 lcd = LCD(i2c)
 
 # thermistor and ldr and ultrasonic
 thermistor = ADC(27)
 ldr = ADC(26)
-
 TRIG = Pin(19, Pin.OUT)
 ECHO = Pin(18, Pin.IN)
 
 # button and led
 button = Pin(15, Pin.IN)
-
 led = Pin(16, Pin.OUT)
 
 # config
@@ -48,6 +41,7 @@ last_email_time = 0
 
 #start with normal mode
 mode = "NORMAL"
+
 # current button and current ldr value
 last_button = button.value()
 baseline_light = ldr.read_u16()
@@ -73,7 +67,7 @@ while True:
     # normal mode
     if mode == "NORMAL":
         celsius, fahrenheit = read_temp(thermistor)
-        led.value(1)
+        led.value(0)
         lcd.clear()
         # lcd display temperature alternate between C and F
         lcd.write(0, 0, "T:{:.1f}C".format(celsius))
